5ab4d4fc5f18814bb47429f695dfd6c6bb310f71
Parth Patel <1498602@TCS.com>
2018-12-17 19:01:13 +0530

    2

--- route/index.js
+++ route/index.js
@@ -1,4 +1,13 @@
 var amfi = require('amfinav');
+var admin = require("firebase-admin");
+var serviceAccount = require("auth/serviceAccountKey.json");
+
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://flutterdemo1-2480a.firebaseio.com"
+});
+
 
 var amfiNavs = new amfi();
 amfiNavs.on('dataready', function (res) {
